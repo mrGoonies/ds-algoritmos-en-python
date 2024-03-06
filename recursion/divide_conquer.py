748c06ee2bf437c95117279536bc0b3217b7c334
mrGoonies <gooniesdev@gmail.com>
2024-03-06 11:40:20 -0300

    Buscando el num mayor dentro de una lista usando recursion

--- recursion/divide_conquer.py
+++ recursion/divide_conquer.py
@@ -18,6 +18,20 @@ def sum_recursion(lst: list[int], value: int=0) -> int:
 
     return sum_recursion(lst, current_value)
 
+def get_higher_num(lst: list[int], current_value=0, current_counter: int = 0) -> int:
+    higher_num = current_value
+    counter = current_counter
+
+    if not lst or counter == len(lst):
+        return higher_num
+
+    if lst[counter] > higher_num:
+        higher_num = lst[counter]
+
+    counter += 1
+
+    return get_higher_num(lst, higher_num, counter)
+
 
 if __name__ == "__main__":
     print("Resultado de una función sin recursión:")
@@ -25,4 +39,7 @@ if __name__ == "__main__":
     
     print("Resultado de una función con recursión:")
     print("\t>> ", sum_recursion([1, 2, 3, 4, 5]))
+
+    print("Resultado del valor más alto de una lista usando recursión es:")
+    print("\t>> ", get_higher_num([10, -5, 100, 3, -23, 1]))
 
